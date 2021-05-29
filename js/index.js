52e1bd4962f020cba903e87a88cf0663083f06ed
Sr.B <bruno932151@gmail.com>
2021-05-28 19:09:12 -0500

    agregando funcionalidad

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+const header = document.getElementById('header')
+const btnRed = document.getElementById('red')
+let redAlert = btnRed.addEventListener('click', Hola)
+
+
+function Hola () {
+
+    alert('oh si nena')
+    // console.log('que pasa')
+}
+
+// const showup = addEventListener()
